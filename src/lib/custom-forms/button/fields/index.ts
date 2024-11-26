5f4e995b3627809649ecd10820b9520f9fc3dd2e
komeilm76 <komeilmohammadian1376.web@gmail.com>
2024-11-26 23:03:30 +0330

    feat:auto-create-new-generator-repository

--- src/lib/custom-forms/button/fields/index.ts
+++ src/lib/custom-forms/button/fields/index.ts
@@ -1,6 +1,6 @@
-import service from '../../../service';	
-const fields = service.form.makeFieldsMap([	
- // insert fields here	
-]);	
-	
+import service from '../../../service';
+const fields = service.form.makeFieldsMap([
+  // insert fields here
+]);
+
 export default fields;
--- src/lib/custom-forms/button/index.ts
+++ src/lib/custom-forms/button/index.ts
@@ -1,10 +1,10 @@
-import service from '../../service';	
-import controller from './controller';	
-import fields from './fields';	
-const form = service.form.makeForm('create-template', fields);	
-form.finish.subscribe((observer) => {	
-  controller(observer);	
-});	
-export default {	
-  form,	
+import service from '../../service';
+import controller from './controller';
+import fields from './fields';
+const form = service.form.makeForm('create-template', fields);
+form.finish.subscribe((observer) => {
+  controller(observer);
+});
+export default {
+  form,
 };
